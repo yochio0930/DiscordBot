bb67c2b167b78063fe405a59458e71a141c5bcce
yochio0930 <51879170+yochio0930@users.noreply.github.com>
2019-09-19 00:46:32 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Form/ComandLibrary.cs
@@ -0,0 +1,95 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Weather_Bot.Form
+{
+	public class ComandLibrary : ModuleBase
+	{
+		string[] mapLibrary = {
+			"海岸線",
+			"高層タワー",
+			"領事館",
+			"銀行",
+			"クラブハウス",
+			"オレゴン",
+			"国境",
+			"ヴィラ",
+			"要塞",
+		 };
+
+		/// <summary>
+		/// [start]とコメントされたときのレスポンス
+		/// </summary>
+		/// <returns>チーム分けを行った結果</returns>
+		[Command("start")]
+		public async Task Start(params string[] person)
+		{
+			var index = person.Length / 2;
+
+			var groupA = new ArrayList();
+			var groupB = new ArrayList();
+
+			for (int i = 0; i < person.Length; i++)
+			{
+				var teamNum = new Random().Next(2);
+				if (teamNum == 1
+					&& groupA.Count < index 
+					&& groupB.Count >= index)
+				{
+					groupA.Add(person[i]);
+				}
+				else
+				{
+					groupB.Add(person[i]);
+				}
+			}
+
+			var strGroupA = string.Join(" ", Enumerable.Range(0, groupA.Count).Select(x => groupA[x]));
+			var strGroupB = string.Join(" ", Enumerable.Range(0, groupB.Count).Select(x => groupB[x]));
+
+			var embed = new EmbedBuilder();
+			embed.WithTitle("♪結果☆");
+			embed.WithDescription("チームA : " + strGroupA + "\nチームB : " + strGroupB);
+
+
+			await ReplyAsync(embed: embed.Build());
+		}
+
+		/// <summary>
+		/// Mapを選択します。
+		/// </summary>
+		/// <returns>ランダムで選択されたマップ</returns>
+		[Command("map")]
+		public async Task Map()
+		{
+			var rnd = new Random().Next(mapLibrary.Length);
+			var result = mapLibrary[rnd];
+
+			var embed = new EmbedBuilder();
+			embed.WithTitle("マップ");
+			embed.WithDescription(result);
+
+
+			await ReplyAsync(embed: embed.Build());
+		}
+
+		[Command("lp")]
+		public async Task Lp()
+		{
+			var result = "a";
+			await ReplyAsync(result);
+		}
+
+		/// <summary>
+		/// マップ選択
+		/// </summary>
+		private string SelectMap => mapLibrary[new Random().Next(mapLibrary.Length)];
+
+	}
+}
+
---
+++ ConsoleApp1/Form/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Discord;
+
+namespace Weather_Bot.Form
+{
+	class Program
+	{
+		public static DiscordSocketClient client;
+		public static CommandService commands;
+		public static IServiceProvider services;
+
+		static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+		/// <summary>
+		/// 起動時処理
+		/// </summary>
+		/// <returns></returns>
+		public async Task MainAsync()
+		{
+			client = new DiscordSocketClient();
+			commands = new CommandService();
+			services = new ServiceCollection().BuildServiceProvider();
+			client.MessageReceived += CommandRecieved;
+
+			client.Log += Log;
+			string token = "NDE1ODA1MDI5OTU0NDg2Mjgz.Dr7JzA.1nP0k9TR-oLaKX2kg79dIr5GBjs";
+			await commands.AddModulesAsync(Assembly.GetEntryAssembly(),services);
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+			await Task.Delay(-1);
+		}
+
+		/// <summary>
+		/// メッセージの受信処理
+		/// </summary>
+		/// <param name="msgParam"></param>
+		/// <returns></returns>
+		private async Task CommandRecieved(SocketMessage messageParam)
+		{
+			var message = messageParam as SocketUserMessage;
+			Console.WriteLine("{0} {1}→{2}", message.Channel.Name, message.Author.Username, message);
+
+			if (message == null) { return; }
+			// コメントがユーザーかBotかの判定
+			if (message.Author.IsBot) { return; }
+
+			int argPos = 0;
+
+			// コマンドかどうか判定（今回は、「.」で判定）
+			if (!(message.HasCharPrefix('.', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) { return; }
+
+			var context = new CommandContext(client, message);
+
+			// 実行
+			var result = await commands.ExecuteAsync(context, argPos, services);
+
+			//実行できなかった場合
+			if (!result.IsSuccess) { await context.Channel.SendMessageAsync("何言ってるかわからないよー(´・ω・`)"); }
+		}
+
+		/// <summary>
+		/// コンソール表示処理
+		/// </summary>
+		/// <param name="msg"></param>
+		/// <returns></returns>
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
+
